d114e4699a8d46b93539d8206cffbcba6590366d
Takeda <wind.ware1203@gmail.com>
2020-12-09 21:38:45 +0800

    update

--- AbcountingvTwo.java
+++ AbcountingvTwo.java
@@ -10,22 +10,35 @@ public class AbcountingvTwo
         Scanner buffer = new Scanner(System.in);
         SecureRandom sec = new SecureRandom();
         int tmp=0; // how many letters are there
-        int[] numArr = new int[26];
+        int[] numUArr = new int[26];
+        int[] numLArr = new int[26];
         tmp = buffer.nextInt();
         System.out.print("Letter:");
         for(int i = 0; i <tmp ;i++)
         {
             char ccc;
-            ccc = (char)(sec.nextInt(26)+97);
+            SecureRandom uOrL = new SecureRandom();
+            boolean flag = uOrL.nextBoolean();
+            if(flag)
+                ccc = (char)(sec.nextInt(26)+97);
+            else
+                ccc = (char)(sec.nextInt(26)+65);
             System.out.print(ccc+" ");
-            int add=(int)ccc-97;
-            numArr[add]++;
+            if(flag)
+                numLArr[(int)ccc-97]++;//
+            else
+                numUArr[(int)ccc-65]++;//
         }
         System.out.println("");
         System.out.println("");
         for(int i = 0; i <26 ;i++)
         {
-            System.out.print((char)(i+97)+":"+numArr[i]+"\t");
+            System.out.print((char)(i+65)+":"+numUArr[i]+"\t");
+            if((i-97)%4==0)System.out.println("");
+        }
+        for(int i = 0; i <26 ;i++)
+        {
+            System.out.print((char)(i+97)+":"+numLArr[i]+"\t");
             if((i-97)%4==0)System.out.println("");
         }
         buffer.close();
